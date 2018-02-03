c980c8e0d95982e1ea7a5bf58186966d2714606e
masaiwk <34976335+masaiwk@users.noreply.github.com>
2018-02-04 01:05:12 +0900

    Create createdb.sql

---
+++ createdb.sql
@@ -0,0 +1,14 @@
+USE coinchart;
+
+CREATE TABLE coinchart
+  (id int NOT NULL AUTO_INCREMENT
+  , coinname nvarchar(32)
+  , coinprice decimal(9,5)
+  , coindatetime datetime 
+  , PRIMARY KEY (id)
+  ,
+  );
+GO
+
+CREATE INDEX idx_cc ON coinchart(coinname,coindatetime);
+GO
